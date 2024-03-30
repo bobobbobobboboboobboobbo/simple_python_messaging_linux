8943c7cdb381e4d216bcef0a57bc2b7a708ca354
bobobbobobboboboobboobbo <165218201+bobobbobobboboboobboobbo@users.noreply.github.com>
2024-03-30 23:29:15 +0000

    things

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install the required packages from requirements.txt
+pip install -r requirements.txt
+
+# Run the Python file (replace "your_python_file.py" with the actual name)
+python main.py
--- main.py
+++ main.py
@@ -2,22 +2,3 @@ import random
 import time
 import sys
 import os
-
-def choice ():
-  choices =  ['7','🍋','🍒','🍊','🍇','🍉','🍌','🍓','bar','wild','jackpot']
-  choice_main = random.randint(0,7)
-  choice_higher = random.getrandbits(1)
-  if choice_higher == 1:
-    add = random.randint(1,3)
-    choice_main += add
-  return choice_main
-
-def get_values():
-  choice1 = choice ()
-  choice2 = choice ()
-  choice3 = choice ()
-
-def main ():
-  choices =  ['7','🍋','🍒','🍊','🍇','🍉','🍌','🍓','bar','wild','jackpot']
-  choice_main = random.randint(0,10)
-  print
