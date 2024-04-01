0e9b69663c65a24d336a5887c23736d08af9534b
bobobbobobboboboobboobbo <165218201+bobobbobobboboboobboobbo@users.noreply.github.com>
2024-04-01 19:27:12 +0000

    final final touches

--- client.py
+++ client.py
@@ -1,24 +1,21 @@
 import socket
 
 HOST = input('Host Server: ')  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-def sentence_to_binary_list(sentence):
-    binary_list = []
-    for char in sentence:
-        ascii_code = ord(char)
-        binary_list.append(bin(ascii_code))
-    return binary_list
-
-      
+PORT = 65432      # The port used by the server
 import socket
 
-while True:
-    message = input("Enter your message: ")
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(message.encode('utf-8'))  # Encode the message as UTF-8 bytes
-        data = s.recv(1024)
-        print('Received', repr(data.decode('utf-8')))  # Decode the received data
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
 
-    
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        # Send message to server
+        message = input("Enter message to send to server: ")
+        s.sendall(message.encode('utf-8'))
+
+        # Receive message from server
+        data = s.recv(1024)
+        if not data:
+            break
+        print('____________________________\n'f"Received from server: {data.decode('utf-8')}")
--- server.py
+++ server.py
@@ -2,16 +2,24 @@ import socket
 
 HOST = ''  # Leave blank for all interfaces
 PORT = 65432  # Port remains the same
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(f"Received message: {data.decode('utf-8')}")  # Decode and print
-                conn.sendall(data)
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            # Receive message from client
+            data = conn.recv(1024)
+            if not data:
+                break
+            print('______________________\n' f"Received from client: {data.decode('utf-8')}")
+
+            # Send message to client
+            message = input("Enter message to send to client: ")
+            conn.sendall(message.encode('utf-8'))
