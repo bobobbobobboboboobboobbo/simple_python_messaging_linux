af5c341d0a6f98b28b4892335aae4685580b466a
bobobbobobboboboobboobbo <165218201+bobobbobobboboboobboobbo@users.noreply.github.com>
2024-04-01 20:02:39 +0000

    the code to decide to use client or server setup

--- init.sh
+++ init.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 # Install the required packages from requirements.txt
-pip install -r requirements.txt
 
-# Run the Python file (replace "your_python_file.py" with the actual name)
-python server.py
+pip install -r requirements.txt
+#!/bin/bash
+clear
+# Prompt the user for input
+read -p "Enter (1) for server or (2) for client: " user_input
+
+# Check if the input is valid
+if [[ $user_input -eq 1 ]]; then
+  # Run file1.py
+  python server.py
+elif [[ $user_input -eq 2 ]]; then
+  # Run file2.py
+  python client.py
+else
+  echo "Invalid input. Please enter 1 or 2."
+fi
