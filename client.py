7db8ef3c5a7edf9792732f8e273c4601325f9be1
bobobbobobboboboobboobbo <165218201+bobobbobobboboboobboobbo@users.noreply.github.com>
2024-04-13 14:50:03 -0400

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ HOST = input('Host Server: ')  # The server's hostname or IP address
 PORT = 65432      # The port used by the server
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = input("host IP Adress: "  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
