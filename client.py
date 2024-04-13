bf9a95ee1bd62df67fcb217f6bda293fc563b44f
bobobbobobboboboobboobbo <165218201+bobobbobobboboboobboobbo@users.noreply.github.com>
2024-04-13 15:25:56 -0400

    Update client.py
    I had an extra line asking for Ip address for the host server weird huh.

--- client.py
+++ client.py
@@ -4,8 +4,6 @@ HOST = input('Host Server: ')  # The server's hostname or IP address
 PORT = 65432      # The port used by the server
 import socket
 
-HOST = input("host IP Adress: ")  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
